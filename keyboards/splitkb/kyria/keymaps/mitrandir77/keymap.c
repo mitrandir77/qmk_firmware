996799a00b1236de29c14699cf8d23326c7b9cb0
Mateusz Kwapich <mitrandir@fb.com>
2022-05-09 16:57:08 +0100

    make rotational encoder press play/pause button
    It's close to volume and left hand.

--- keyboards/splitkb/kyria/keymaps/mitrandir77/keymap.c
+++ keyboards/splitkb/kyria/keymaps/mitrandir77/keymap.c
@@ -62,7 +62,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      KC_TAB  , KC_Q ,  KC_W   ,  KC_E  ,   KC_R ,   KC_T ,                                         KC_Y,   KC_U ,  KC_I ,   KC_O ,  KC_P , KC_MINS,
      CTL_ESC , KC_A ,  KC_S   ,  KC_D  ,   KC_F ,   KC_G ,                                         KC_H,   KC_J ,  KC_K ,   KC_L ,KC_SCLN,CTL_QUOT,
      KC_LSFT , KC_Z ,  KC_X   ,  KC_C  ,   KC_V ,   KC_B , CMD_LBRC, KC_LCMD,   FKEYS   , CMD_RBRC,KC_N,   KC_M ,KC_COMM, KC_DOT ,KC_SLSH, KC_RSFT,
-                                ADJUST , KC_LALT,  KC_DEL, KC_BSPC,  SYM   ,    KC_ENT  , KC_SPC , NAV ,  KC_RALT, KC_APP
+                                KC_MPLY, KC_LALT,  KC_DEL, KC_BSPC,  SYM   ,    KC_ENT  , KC_SPC , NAV ,  KC_RALT, ADJUST
     ),
 
 /*
